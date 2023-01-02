57a0be26f6d3253b831171a116b5aff9553a15dc
pavelgavrilenkoqa <pavel.g@avantajprim.com>
2023-01-02 14:53:10 +0200

    -- универсальный старт для андроид устройств средствами вшитого adb

--- main/global_auto_agr.py
+++ main/global_auto_agr.py
@@ -8,11 +8,13 @@ AUTO_SETUP_ARG = {
     'path': os.getcwd(),
     'logdir': True,
     # 'logdir': './log',
-    'devices': 'android://127.0.0.1:5037/08111JEC214267?cap_method=MINICAP&&ori_method=MINICAPORI&&touch_method=MAXTOUCH', ### Pixel
+    'devices': 'android:///', ### универсальный ключ для запуска на текущем подключенном устройстве
+    #127.0.0.1:5037/08111JEC214267?cap_method=MINICAP&&ori_method=MINICAPORI&&touch_method=MAXTOUCH' ### Pixel personal start
     'project_root': project_dir
 }
 
 if __name__ == "__main__":
     print('go')
-    path = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'log')
+    path = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'lol')
     print(path)
+
