fa94810508a279c3b801474e2b0655c15d8b6af0
pavelgavrilenkoqa <pavel.g@avantajprim.com>
2023-01-03 10:43:35 +0200

    -- фича прохождения туториала + предустановка языка для основного рана

--- main/main.py
+++ main/main.py
@@ -5,11 +5,13 @@ from marker_feature import market_main
 from setting_feature import setting_main
 from daily_feature import daily_main
 from puzzle_tab_feature import puzzle_tab_main
+from tutorial_feature import tutorial_main
 from try_test_method import remove_txt_result_file
 
 
 if __name__ == "__main__":
     remove_txt_result_file()
+    tutorial_main.run_tutorial_feature()
     market_main.run_market_feature()
     setting_main.run_setting_feature()
     daily_main.run_daily_feature()
---
+++ main/tutorial_feature/tutorial_main.py
@@ -0,0 +1,139 @@
+# -*- encoding=utf8 -*-
+__author__ = "QA PG"
+
+from airtest.core.api import *
+from airtest.report.report import simple_report
+from global_auto_agr import AUTO_SETUP_ARG
+from try_test_method import run_logger, remove_log_dir
+
+
+def test_politic_accept():
+    sleep(2)
+    touch(Template(
+        r"tpl1672664808761.png",
+        record_pos=(-0.003, 0.328),
+        resolution=(1080, 2340))
+    )
+    sleep(6)
+    assert_exists(Template(
+        r"tpl1672664849254.png",
+        record_pos=(-0.003, -0.211),
+        resolution=(1080, 2340)),
+        "Ожидаем решенный пазл туториал после принятия политики"
+    )
+
+
+def test_tutorial_part_1():
+    touch(Template(
+        r"tpl1672664808761.png",
+        record_pos=(-0.003, 0.328),
+        resolution=(1080, 2340))
+    )
+    sleep(6)
+    assert_exists(Template(
+        r"tpl1672664959749.png",
+        record_pos=(0.271, 0.71),
+        resolution=(1080, 2340)),
+        "Ожидаем появления части панели элементов"
+    )
+    sleep(5)
+    touch(Template(
+        r"tpl1672665048156.png",
+        record_pos=(-0.321, 1.038),
+        resolution=(1080, 2340))
+    )
+    sleep(4)
+    assert_exists(Template(
+        r"tpl1672664849254.png",
+        record_pos=(-0.003, -0.211),
+        resolution=(1080, 2340)),
+        "Ожидаем решенный пазл туториал после первой части тутора"
+    )
+
+
+def test_tutorial_part_2():
+    touch(Template(
+        r"tpl1672664808761.png",
+        record_pos=(-0.003, 0.328),
+        resolution=(1080, 2340))
+    )
+    sleep(6)
+    assert_exists(Template(
+        r"tpl1672665146926.png",
+        record_pos=(0.224, -0.213),
+        resolution=(1080, 2340)),
+        "Ожидаем пустое поле и наличие верхней части панели элементов"
+    )
+    touch(Template(
+        r"tpl1672665048156.png",
+        record_pos=(-0.321, 1.038),
+        resolution=(1080, 2340))
+    )
+    assert_exists(Template(
+        r"tpl1672664849254.png",
+        record_pos=(-0.003, -0.211),
+        resolution=(1080, 2340)),
+        "Ожидаем решенный пазл туториал после второй чатси тутора"
+    )
+
+
+def test_tutorial_part_3():
+    touch(Template(
+        r"tpl1672664808761.png",
+        record_pos=(-0.003, 0.328),
+        resolution=(1080, 2340))
+    )
+    sleep(7)
+    assert_exists(Template(
+        r"tpl1672665342996.png",
+        record_pos=(0.014, -0.303),
+        resolution=(1080, 2340)),
+        "Ожидаем отображение арта окончания тутора входа в пазл"
+    )
+
+
+def test_language_check():
+    sleep(2)
+    touch(Template(r"tpl1672665432382.png", record_pos=(-0.381, 0.969), resolution=(1080, 2340)))
+    sleep(2)
+    touch(Template(r"tpl1672665432382.png", record_pos=(-0.381, 0.969), resolution=(1080, 2340)))
+    sleep(2)
+    touch(Template(r"tpl1672665460512.png", record_pos=(-0.403, -0.857), resolution=(1080, 2340)))
+    ### Проверяем подключеный язык, при необходимости переключаем на английский
+    if not exists(Template(r"tpl1672665487700.png", record_pos=(-0.213, -0.015), resolution=(1080, 2340))):
+        touch(Template(r"tpl1672665535609.png", record_pos=(0.265, -0.057), resolution=(1080, 2340)))
+        touch(Template(r"tpl1672665487700.png", record_pos=(-0.213, -0.015), resolution=(1080, 2340)))
+        sleep(2)
+        assert_exists(Template(
+            r"tpl1672665487700.png",
+            record_pos=(-0.213, -0.015),
+            resolution=(1080, 2340)),
+            "Ожидаем переключение на английский язык"
+        )
+    ### Выполняем активное действие для сохранения статуса прохождения обучения
+    touch(Template(r"tpl1672665432382.png", record_pos=(-0.381, 0.969), resolution=(1080, 2340)))
+    sleep(2)
+    touch(Template(r"tpl1672671623360.png", record_pos=(0.007, -0.537), resolution=(1080, 2340)))
+    touch(Template(r"tpl1672671633756.png", record_pos=(-0.003, -0.476), resolution=(1080, 2340)))
+    touch(Template(r"tpl1672671642469.png", record_pos=(-0.003, 0.531), resolution=(1080, 2340)))
+    sleep(6)
+
+
+def run_tutorial_feature():
+    remove_log_dir('tutorial_feature')
+    auto_setup(
+        __file__,
+        logdir=AUTO_SETUP_ARG.get('logdir'),
+        devices=[AUTO_SETUP_ARG.get('devices'), ],
+        project_root=AUTO_SETUP_ARG.get('project_root'),
+    )
+    clear_app("com.jollyco.jbpuzzleadventure")
+    start_app("com.jollyco.jbpuzzleadventure")
+    run_logger(test_politic_accept)
+    run_logger(test_tutorial_part_1)
+    run_logger(test_tutorial_part_2)
+    run_logger(test_tutorial_part_3)
+    run_logger(test_language_check)
+    stop_app("com.jollyco.jbpuzzleadventure")
+    simple_report(__file__, logpath=True, output='html_report/tutorial.html')
+
