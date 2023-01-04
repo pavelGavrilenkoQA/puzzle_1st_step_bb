b00dcdd658fc185dd43d266b466b113053ad7f90
pavelgavrilenkoqa <pavel.g@avantajprim.com>
2023-01-04 11:52:37 +0200

    -- универсализация путей под Unix

--- main/try_test_method.py
+++ main/try_test_method.py
@@ -1,16 +1,19 @@
 import os
 import shutil
+import pathlib
 from global_auto_agr import AUTO_SETUP_ARG
 
 def run_logger(method):
     '''Выполняется тестовый метод, при удачном выполнении пишет в пройденные'''
     '''при падении пишет в падения вместе с исключением'''
+    PATH_TO_PASSED = pathlib.Path(f'{AUTO_SETUP_ARG.get("project_root")}', 'passed.txt')
+    PATH_TO_FAILED = pathlib.Path(f'{AUTO_SETUP_ARG.get("project_root")}', 'failed.txt')
     try:
         method()
-        with open(f'{AUTO_SETUP_ARG.get("project_root")}/passed.txt', 'a+') as f:
+        with open(f'{PATH_TO_PASSED}', 'a+') as f:
             f.write(f'{method.__name__} --- is passed\n')
     except Exception as e:
-        with open(f'{AUTO_SETUP_ARG.get("project_root")}/failed.txt', 'a+') as f:
+        with open(f'{PATH_TO_FAILED}', 'a+') as f:
             f.write(f'{method.__name__} --- is failed, detail: {e}\n')
 
 
@@ -30,6 +33,6 @@ def remove_log_dir(feature_dir):
     '''Чистит внутрению папку лога перед запуском фичи'''
     '''Передать str название папки фичи'''
     try:
-        shutil.rmtree(f'{AUTO_SETUP_ARG.get("project_root")}\{feature_dir}\log')
+        shutil.rmtree(pathlib.Path(f'{AUTO_SETUP_ARG.get("project_root")}', f'{feature_dir}', 'log'))
     except Exception as e:
         print(f'проблема с удаление лог дира {feature_dir} подробности {e}')
